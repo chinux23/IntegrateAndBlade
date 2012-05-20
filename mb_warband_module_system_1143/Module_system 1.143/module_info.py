b22777780b9a51d40250ea3179a5f4ef34abe571
Chen Huang <chen_huang@apple.com>
2012-05-20 14:56:45 -0700

    fix build error - wrong directory

--- mb_warband_module_system_1143/Module_system 1.143/module_info.py
+++ mb_warband_module_system_1143/Module_system 1.143/module_info.py
@@ -2,5 +2,5 @@
 # Point export_dir to the folder you will be keeping your module
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
 
-export_dir = "../../../Diplo+PBOD+Floris+SourceMerge"
+export_dir = "../../../Diplo+PBOD+Floris+SourceMerge/"
 #export_dir = "C:/Program Files/Mount&Blade/Modules/Native/"
